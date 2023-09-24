ffc4527e4f5e7215153c2637e0c78c39531bdcf1
jgutierrez287 <112670503+jgutierrez287@users.noreply.github.com>
2023-09-24 13:23:10 -0700

    Update Main.java
    redid the driver add to allow users to interact with a menu. the add class and remove class still new work

--- Main.java
+++ Main.java
@@ -1,47 +1,115 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         HashMap<Integer, CourseTree> courses = new HashMap<Integer, CourseTree>();
         HashMap<Integer, List<Integer>> students = new HashMap<Integer, List<Integer>>();
-
-        // Written for testing purposes
-        int[] times = {1130, 1245, 1215, 1330, 1300, 1415, 1345, 1500}; // times are in sets of 2
-        for(int i = 0; i < 10; i++){
-            int courseCode;
-            do {
-                courseCode = (int)(Math.random()*8999) + 1000; // random number from 1000 to 9999
-            } while(!courses.containsKey(courseCode)); // reroll code until a unique course code is generated
-            CourseTree tree = new CourseTree(times[(int)(i / 3)], times[(int)(i / 3)+1]); // e.g. i=5, ST= 1215, ET= 1330
-            courses.put(courseCode, tree);
-        }
-        
-        for(int i = 0; i < 10; i++){ // 10 with prints, 80 in the end
-            //make a student id
-            int id;
-            do{
-                id = (int)(Math.random()*89999) + 10000; // random number from 10000 to 99999
-            } while(!students.containsKey(id)); // reroll id until a unique student id is generated
-            students.put(id, new ArrayList<Integer>());
-            //decide which of the 10 classes they will try to add
+        Scanner input = new Scanner(System.in);
+        int choice;
+        do{
+            System.out.println("Menu");
+            System.out.println("1:Enroll in school");
+            System.out.println("2:Add classes");
+            System.out.println("3:Remove classes");
+            System.out.println("4:Print class schedule");
+            System.out.println("-1:Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+            
+            switch(choice){
+                case 1:
+                    int studentId = createStudentId();
+                    students.put(studentId, new ArrayList<Integer>());
+                    System.out.println("Enrolled in School. Your student ID is: " + studentId);
+                    break;
+                case 2:
+                    System.out.println("Enter your student ID: ");
+                    int studentAdd =input.nextInt();
+                    input.nextLine();
+                    if(students.containsKey(studentAdd)){
+                        addClass(courses,students, studentAdd);
+                    }else{
+                        System.out.println("Invalid Student ID");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter your student ID: ");
+                    int studentRemove =input.nextInt();
+                    input.nextLine();
+                    if(students.containsKey(studentRemove)){
+                        removeClass(students,studentRemove);
+                    }else{
+                        System.out.println("Invalid Student ID");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter your student ID: ");
+                    int studentPrint =input.nextInt();
+                    input.nextLine();
+                    if(students.containsKey(studentPrint)){
+                        printSchedule(students,studentPrint);
+                    }else{
+                        System.out.println("Invalid Student ID");
+                    }
+                    break;
+                case -1:
+                    System.out.println("Exitting program");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid number.Enter another choice");
+            }
+        }while(choice != 0);
+        input.close();
+    }
+    public static int createStudentId(){
+        return(int)(Math.random()* 89999) + 10000;
+    }
+    public static void addClass(HashMap<Integer, CourseTree> courses, HashMap<Integer, List<Integer>> students, int studentId){
             for(int courseCode : courses.keySet()){
-                if((int)(Math.random()*2) == 0) // 50% chance of trying to enroll
-                {
-                    CourseTree course = courses.get(courseCode); // course we are attempting to enroll in
-                    for(int existingCourseCode : students.get(id))
-                    {
+                if((int)(Math.random() * 2) == 0){
+                    CourseTree course = courses.get(courseCode);
+                    boolean timeConflictExists = false;
+                    for(int existingCourseCode : students.get(studentId)){
                         CourseTree existingCourse = courses.get(existingCourseCode);
-                        if(!existingCourse.timeConflict(course))
-                            students.get(id).add(courseCode);
+                        if(existingCourse.timeConflict(course)){
+                            timeConflictExists = true;
+                            System.out.println("Failed to add course " + courseCode + " due to time conflict with " + existingCourseCode);
+                            break;
+                        }
+                    }
+                    if(!timeConflictExists){
+                        students.get(studentId).add(courseCode);
+                        System.out.println("Successfully added course");
                     }
                 }
             }
         }
-        // String[] blah = {"Mackenzie", "Jack", "Ryan", "Laura", "Shivam", "Maria", "Selena", "Mike", "Aaron"};
-        // List<String> names = Arrays.asList(blah);
+    public static void removeClass(HashMap<Integer, List<Integer>> students, int studentId){
+        List<Integer> enrolledClasses = students.get(studentId);
+        if(!enrolledClasses.isEmpty()){
+            int removeIndex = (int)(Math.random()*enrolledClasses.size());
+            int removeCourseCode = enrolledClasses.remove(removeIndex);
+            System.out.println("Successfully removed course");
+        }else{
+            System.out.println("Class schedule is empty");
+        }
+        
+    }
+    public static void printSchedule(HashMap<Integer, List<Integer>> students, int studentId){
+        List<Integer> enrolledClasses = students.get(studentId);
+        if(!enrolledClasses.isEmpty()){
+            System.out.print("Your schedule:");
+            for(int courseCode : enrolledClasses){
+                System.out.println("Course: " + courseCode);
+            }
+        }else{
+            System.out.println("Class Schedule is empty");
+        }
+        
     }
     
-    
-}
+    }
