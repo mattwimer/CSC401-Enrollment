e1dabdccd9ed7b750597ab833f67ac93b869fed0
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-09-23 20:29:14 -0700

    Need a specific tree for courses to store time

---
+++ CourseTree.java
@@ -0,0 +1,3 @@
+public class CourseTree extends AVLTree{
+    
+}
--- Main.java
+++ Main.java
@@ -1,3 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args){
+        HashMap<Integer, AVLTree> courses = new HashMap<Integer, AVLTree>();
+        HashMap<Integer, String> students = new HashMap<Integer, String>();
+
+        // Written for testing purposes
+        for(int i = 0; i < 3; i++){
+            int courseCode;
+            do {
+                courseCode = (int)(Math.random()*8999) + 1000; // random number from 1000 to 9999
+            } while(!courses.containsKey(courseCode));
+            AVLTree tree = new AVLTree();
+            courses.put(courseCode, tree);
+        }
+        
+        String[] blah = {"Mackenzie", "Jack", "Ryan", "Laura", "Shivam", "Maria", "Selena", "Mike", "Aaron"};
+        List<String> names = Arrays.asList(blah);
+    }
+    
     
 }
